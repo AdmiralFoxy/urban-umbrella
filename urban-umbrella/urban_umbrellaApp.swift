a08ad61c76665c1ba031ecb5ca685d6e407ca578
Stanislav <avramss15@gmail.com>
2021-08-07 13:31:13 +0300

    Create project

---
+++ urban-umbrella/urban_umbrellaApp.swift
@@ -0,0 +1,17 @@
+//
+//  urban_umbrellaApp.swift
+//  urban-umbrella
+//
+//  Created by Stanislav on 07.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct urban_umbrellaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
